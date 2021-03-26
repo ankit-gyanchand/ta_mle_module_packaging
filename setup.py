fa6b2a824299d2640e9cbd473bba944eca9bdf2d
Ankit Gyan Chand <ankit.gyanchand@tigeranalytics.com>
2021-03-26 15:47:40 +0530

    updated files to make package ready for PyPi

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
 from setuptools import setup
 
 setup(
-    name='helloworld',
-    version='0.0.1',
-    description='description of say hello.',
-    py_modules=['say_hello'],
-    package_dir={'': 'src'},
+    name="helloworld",
+    version="0.0.1",
+    description="description of say hello.",
+    py_modules=["say_hello"],
+    package_dir={"": "src"},
     classifiers=[
-        'programming language :: Python :: 3',
-        'programming language :: Python :: 3.6',
-        'programming language :: Python :: 3.7',
-        'License :: MIT License :: simple and permissive',
-        'Operating System :: OS independent',
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
-    install_requires=[
-        
-    ],
+    install_requires=[],
     extras_require={
-        'dev': [
-            'pytest>=3.7',
+        "dev": [
+            "pytest>=3.7",
         ]
     },
-    url='www.google.com',
-    author='Ankit',
-    author_email='moralankit@gmail.com',
-    
+    url="https://github.com/ankit-gyanchand/ta_mle_module_packaging",
+    author="ankit_300",
+    author_email="ankit.gyanchand@tigeranalytics.com",
 )
-
